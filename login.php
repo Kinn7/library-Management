29d32449a3e6c31db26c54a39bbcdad396f24818
Kinn7 <kidus.berhanu.gh9353@gmail.com>
2022-05-28 12:44:35 +0300

    add login ui

---
+++ login.php
@@ -0,0 +1,78 @@
+<?php
+include "classes/connection.php";
+$connect = new Db();
+$connect->login();
+// session_start();
+// $_SESSION['user'] = '';
+// $_SESSION['userId'] = '';
+// include "classes/connection.php";
+// $msg = "";
+// if(isset($_POST['submit'])){
+//     $connect = new Db();
+
+//     $name = mysqli_real_escape_string($connect->connect(),$_POST['name']);
+//     $pass = mysqli_real_escape_string($connect->connect(),$_POST['password']);
+//     $pass = md5($pass);
+    
+//     $query = "SELECT * FROM admin WHERE name='$name' and password='$pass'";
+//     $res = $connect->connect()->query($query);
+
+//     if($res->num_rows){
+//         $admin = $res->fetch_object();
+//         $_SESSION['userId'] = $admin->id;
+//         $_SESSION['user'] = $admin->name;
+//         header("Location: dashboard.php");
+//     }elseif(empty($_POST['name']) || empty($_POST['password'])){
+//         $msg = "Field empty";
+//     }
+//     else{
+//         $msg = "Wrong user or password";
+//     }
+
+    
+
+// }
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/bootstrap.js"></script>
+    <title>Login</title>
+</head>
+
+<body>
+   <!-- <form method="POST" enctype="multipart/form-data"> -->
+    <div class="container">
+	<div class="row">
+		<form method="post" class="form-horizontal col-md-6 col-md-offset-3">
+			<h2>Create Operation in CRUD Application</h2>
+			<div class="form-group">
+			    <label for="name" class="col-sm-2 control-label">Name</label>
+			    <div class="col-sm-10">
+			      <input type="text" name="name"  class="form-control" id="password" placeholder="user name" />
+			    </div>
+			</div>
+ 
+			<div class="form-group">
+			    <label for="password" class="col-sm-2 control-label">Password</label>
+			    <div class="col-sm-10">
+			      <input type="password" name="password"  class="form-control" id="password" placeholder="password" />
+			    </div>
+			</div>
+			<input type="submit" name="submit" class="btn btn-primary col-md-10 col-md-offset-10" value="submit" />
+            <div class="btn-danger col-md-10"><?php echo  $connect->msg ?></div>
+		</form>
+	</div>
+</div>
+
+</body>
+</html>
+    <!-- </form>
+</body>
+</html>
