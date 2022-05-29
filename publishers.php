2340aa535f174bf0dc11808a4cd62fab8efdad67
Kinn7 <kidus.berhanu.gh9353@gmail.com>
2022-05-29 21:23:39 +0300

    display list of publishers

--- publishers.php
+++ publishers.php
@@ -3,11 +3,29 @@
 $currentPage = "publishers.php";
 include "navigation.php";
 
+$pubs = new Db();
+
 ?>
 
 <div class="main">
-<p>
-    publishers
+    <h2 class="display3 d-flex justify-content-center mb-5">List of Publishers</h2>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Publisher</th>
+                <th>Country</th>
+                <th>Headquarter</th>
+                <th>No of branches</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <?php $pubs->getPublishers(); ?>
+            </tr>
+        </tbody>
+
+    </table>
+    
 </p>
 </div>
 
