9d14ea00f3ec9c76d483e0b24b74ac70424b376b
Kinn7 <kidus.berhanu.gh9353@gmail.com>
2022-06-05 23:46:51 +0300

    add ob_start() function at the start of the script to solve header() problem in other pages

--- navigation.php
+++ navigation.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 session_start();
 include_once "classes/connection.php";
 
@@ -78,21 +79,21 @@ if(!$userId){
             </a>
             <ul class="nav flex-column vertical-nav">
                 <li class="nav-item">
-                    <a   href="dashboard.php" <?php if ($currentPage == "dashboard.php"){ echo "class='nav-link active'"; } ?> >
+                    <a   href="dashboard.php" <?php if ($currentPage == "dashboard.php"){ echo "class='nav-link active'"; }?> >
                         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
                         <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
                     </svg>
                     </i></a>
                 </li>
                 <li class="nav-item">
-                    <a  href="books.php"  <?php if ($currentPage == "books.php"){ echo "class='nav-link active'"; } ?> >
+                    <a  href="books.php"  <?php if ($currentPage == "books.php"){ echo "class='nav-link active'"; }?> >
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
   <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
 </svg>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a  href="publishers.php" <?php if ($currentPage == "publishers.php"){ echo "class='nav-link active'"; }else { echo "class='nav-link'"; } ?> >
+                    <a  href="publishers.php" <?php if ($currentPage == "publishers.php"){ echo "class='nav-link active'"; }else { echo "class='nav-link'"; }?> >
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-printer" viewBox="0 0 16 16">
   <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
   <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z"/>
@@ -100,7 +101,7 @@ if(!$userId){
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a  href="members.php" <?php if ($currentPage == "members.php"){ echo "class='nav-link active'"; } ?> >
+                    <a  href="members.php" <?php if ($currentPage == "members.php"){ echo "class='nav-link active'"; }  ?> >
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
   <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
 </svg>
