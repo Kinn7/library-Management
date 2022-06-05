3a229f3a7569e8b231a2c3d6c209cc4d4566a6b4
Kinn7 <kidus.berhanu.gh9353@gmail.com>
2022-06-05 23:49:25 +0300

    remove quotation bug coming on output

--- createPublisher.php
+++ createPublisher.php
@@ -10,12 +10,13 @@ if(isset($_POST) && (!empty($_POST['pubName']) && !empty($_POST['country']) && !
     $res = $pub->createPublishers($pubName,$country,$headQuarter,$branches);
 
     if($res){
-        $pub->msg =  '"<div class="alert alert-success d-flex align-items-center justify-content-center h6" role="alert">
-        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+        $pub->msg =  "<div class='alert alert-success d-flex align-items-center justify-content-center h6' role='alert'>
+        <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Success:'><use xlink:href='#check-circle-fill'/></svg>
         <div>
           Successfully inserted data
         </div>
-      </div>"';
+      </div>";
+
     }
     // else{
     //     $pub->msg =  "failed to insert data";
