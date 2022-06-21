5b927a64069d7dbd8dcb219d3d92d53b28dfa025
Kinn7 <kidus.berhanu.gh9353@gmail.com>
2022-06-21 16:36:51 +0300

    add page to see information about selected book

---
+++ viewBook.php
@@ -0,0 +1,62 @@
+<?php
+$currentPage = "books.php";
+include "navigation.php";
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    $books = $db->getBookById($id)->fetch_object();
+//    $fetch = $books->fetch_object();
+}
+?>
+
+<div class="main">
+    <div class="container">
+    <div class="row">
+        <div class="col p-3  d-flex justify-content-center">
+            <?php
+                echo "<img src='uploads/$books->book_img'>";
+            ?>
+   
+        </div>
+        </div>
+   
+        <div class="row">
+
+        <!-- <div class="col d-flex justify-content-center">
+                <?php //echo "<h4> $books->title </h4>"; ?>
+            </div> -->
+            <div class="w-100"></div>
+
+        <div class="col d-flex justify-content-center">
+        <?php echo "<h5><strong>Author</strong>: $books->author </h5>"; ?>
+            </div>
+            <div class="w-100"></div>
+
+        <div class="col d-flex justify-content-center">
+        <?php echo "<h5><strong>Isbn</strong>: $books->isbn </h5>"; ?>
+            </div>
+            <div class="w-100"></div>
+
+        <div class="col d-flex justify-content-center">
+        <?php echo "<h5><strong>Availability</strong>: $books->available </h5>"; ?>
+            </div>
+            <div class="w-100"></div>
+
+        <div class="col d-flex justify-content-center">
+        <?php echo "<h5><strong>Quantity</strong>: $books->quantity </h5>"; ?>
+            </div>
+            <div class="w-100"></div>
+
+        <div class="col d-flex justify-content-center">
+        <?php echo "<h5><strong>Price</strong>: $books->price </h5>"; ?>
+            </div>
+            <div class="w-100"></div>
+
+        <div class="col d-flex justify-content-center">
+        <?php echo "<h5><strong>Publisher</strong>: $books->pub_name </h5>"; ?>
+            </div>
+
+        </div>
+    </div>
+
+</div>
+
